4e7aa4aaceae67280f6efcdf0801abc6d7157ec5
Norah Saarman <u6036559@notch320.ipoib.int.chpc.utah.edu>
2025-06-03 17:03:21 -0600

    PCA

--- scripts/00_install_packages.R
+++ scripts/00_install_packages.R
@@ -1,1 +1,13 @@
+# Running list of packages
+install.packages("adegenet")
+install.packages("geosphere")
+install.packages("igraph")
+install.packages("ggplot2")
+install.packages("scatterplot3d")
+install.packages("dplyr")
 
+# Original list of potential packages
+install.packages("hierfstat")
+install.packages("pegas")       
+install.packages("PopGenReport")
+install.packages("poppr")
