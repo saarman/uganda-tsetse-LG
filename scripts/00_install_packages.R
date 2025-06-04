bb327bdb1627074a6139ae319476693b91b86972
Norah Saarman <u6036559@notch320.ipoib.int.chpc.utah.edu>
2025-06-04 17:05:00 -0600

    pca cse

--- scripts/00_install_packages.R
+++ scripts/00_install_packages.R
@@ -5,8 +5,6 @@ install.packages("igraph")
 install.packages("ggplot2")
 install.packages("scatterplot3d")
 install.packages("dplyr")
-
-# Original list of potential packages
 install.packages("hierfstat")
 install.packages("pegas")       
 install.packages("PopGenReport")
